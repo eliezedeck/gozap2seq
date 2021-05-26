4a718debca05ffe7c1ea5099f9d4a88f5968a660
Elie Zedeck RANDRIAMIANDRIRAY <elie.zedeck@gmail.com>
2021-05-26 12:27:53 +0300

    Untested implementation

---
+++ go.mod
@@ -0,0 +1,8 @@
+module gozap2seq
+
+go 1.16
+
+require (
+	github.com/tidwall/gjson v1.8.0
+	go.uber.org/zap v1.17.0
+)
---
+++ logger.go
@@ -0,0 +1,70 @@
+package gozap2seq
+
+import (
+	"bytes"
+	"errors"
+	"io"
+	"net/http"
+
+	"github.com/tidwall/gjson"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+type LogInjector struct {
+	client   *http.Client
+	sequrl   string
+	seqtoken string
+}
+
+func NewLogInjector(url, token string) *LogInjector {
+	return &LogInjector{
+		client:   &http.Client{},
+		sequrl:   url,
+		seqtoken: token,
+	}
+}
+
+func (i *LogInjector) Build(zapconfig zap.Config) *zap.Logger {
+	// SEQ requires that the fields and value format follow these rules
+	zapconfig.EncoderConfig.EncodeTime = zapcore.RFC3339NanoTimeEncoder
+	zapconfig.EncoderConfig.EncodeLevel = zapcore.LowercaseLevelEncoder
+	zapconfig.EncoderConfig.LevelKey = "@l"
+	zapconfig.EncoderConfig.TimeKey = "@t"
+	zapconfig.EncoderConfig.MessageKey = "&mt"
+
+	jsonencoder := zapcore.NewJSONEncoder(zapconfig.EncoderConfig)
+	seqsync := zapcore.AddSync(i)
+
+	return zap.New(zapcore.NewCore(jsonencoder, seqsync, zapconfig.Level.Level()))
+}
+
+func (i *LogInjector) Write(p []byte) (n int, err error) {
+	req, err := http.NewRequest("POST", i.sequrl+"/api/events/raw", bytes.NewBuffer(p))
+	if err != nil {
+		return 0, err
+	}
+
+	if i.seqtoken != "" {
+		req.Header.Set("X-Seq-ApiKey", i.seqtoken)
+	}
+	req.Header.Set("Content-Type", "application/vnd.serilog.clef")
+
+	// Get the response
+	resp, err := i.client.Do(req)
+	if err != nil {
+		return 0, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		// Parse the JSON message
+		content, err := io.ReadAll(resp.Body)
+		if err != nil {
+			return 0, err
+		}
+		value := gjson.GetBytes(content, "Error")
+		return 0, errors.New("upstream error: " + value.String()) // error (with message)
+	}
+	return len(p), nil // success
+}
