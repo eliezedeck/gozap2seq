14cd7b8fde8137a3434c6b8d8910a8ea52a4df0e
Elie Zedeck RANDRIAMIANDRIRAY <elie.zedeck@gmail.com>
2021-05-26 12:56:10 +0300

    Integration test

--- logger_test.go
+++ logger_test.go
@@ -1,10 +1,33 @@
 package gozap2seq
 
-import "testing"
+import (
+	"errors"
+	"testing"
+	"time"
+
+	"go.uber.org/zap"
+)
 
 func TestBadUrl(t *testing.T) {
 	_, err := NewLogInjector("http://///////", "boooo")
 	if err == nil {
 		t.Error("not handling bad URL properly")
 	}
+}
+
+func TestInjectionIntegration(t *testing.T) {
+	injector, err := NewLogInjector("http://localhost:5341", "")
+	if err != nil {
+		t.Error(err)
+	}
+
+	loggerConfig := zap.NewDevelopmentConfig()
+	logger := injector.Build(loggerConfig)
+
+	logger.Debug("Debug message", zap.String("level", "debug"), zap.Bool("ok", true))
+	logger.Info("Info message", zap.String("level", "info"), zap.Binary("binary", []byte("hello")), zap.String("original", "hello"))
+	logger.Warn("Warning message", zap.String("newline", "{\n    \"hello\": \"world\"\n}"))
+	logger.Error("Error message", zap.Error(errors.New("oh no!")))
+
+	time.Sleep(1 * time.Second)
 }
